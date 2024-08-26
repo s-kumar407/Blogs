29e4aa9efce9e200a8f091435fd761bf6f7eaf0f
s-kumar407 <sachinkr407@gmail.com>
2024-08-26 14:14:12 +0530

    tenth commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use(cors({
-  origin:[`${process.env.FRONTEND_URI}`],
+  origin:[`${process.env.FRONTEND_URI}:${process.env.PORT}`],
   methods:["POST","GET","PUT","DELETE"],
   credentials:true
 }));
